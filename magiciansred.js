d8eb862b3b422ec23baf41ea1bafd6e67f6a9b59
Shingo Tamaki <tamaki.shingo@gmail.com>
2016-06-12 20:57:22 +0900

    first commit

---
+++ magiciansred.js
@@ -0,0 +1,24 @@
+module.exports = function(RED) {
+    var irMagician = require('irmagician')
+
+    function play(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        this.on('input', function(msg) {
+            irMagician.play()
+        });
+    }
+    RED.nodes.registerType("irMagician play", play);
+
+    function write(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        this.file = config.file;
+        this.on('input', function(msg) {
+            console.log(config);
+            console.log(this.file);
+            irMagician.write(file)
+        });
+    }
+    RED.nodes.registerType("irMagician write", write);
+}
